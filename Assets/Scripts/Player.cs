62c2a2ed2dd5716a54191e53bbebb9f066510cc8
youngil <youngil@placeb.com>
2023-10-26 11:10:16 +0900

    화면 경계 이동

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 5f;
+
+    Vector2 rawInput;
+
+    [SerializeField] float paddingLeft;
+    [SerializeField] float paddingRight;
+    [SerializeField] float paddingTop;
+    [SerializeField] float paddingBottom;
+
+    Vector2 minBounds;
+    Vector2 maxBounds;
+
+    private void Start()
+    {
+        InitBounds();
+    }
+    void Update()
+    {
+        Move();
+    }
+
+    void InitBounds()
+    {
+        Camera mainCamera = Camera.main;
+        minBounds = mainCamera.ViewportToWorldPoint(Vector2.zero);
+        maxBounds = mainCamera.ViewportToWorldPoint(Vector2.one);
+    }
+
+    private void Move()
+    {
+        Vector3 delta = rawInput * moveSpeed * Time.deltaTime;
+        Vector2 newPos = new Vector2();
+        newPos.x = Mathf.Clamp(transform.position.x + delta.x, minBounds.x + paddingLeft, maxBounds.x - paddingRight);
+        newPos.y = Mathf.Clamp(transform.position.y + delta.y, minBounds.y + paddingBottom, maxBounds.y - paddingTop);
+
+        transform.position = newPos;
+    }
+
+    void OnMove(InputValue value)
+    {
+        rawInput = value.Get<Vector2>();
+        Debug.Log(rawInput);
+    }
+}
