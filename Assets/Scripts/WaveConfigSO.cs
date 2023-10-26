6cde176c9c776daecf6c0cfbfa538e64ac80e38b
youngil <youngil@placeb.com>
2023-10-26 13:59:41 +0900

    wave path

--- Assets/Scripts/WaveConfigSO.cs
+++ Assets/Scripts/WaveConfigSO.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Wave Config", fileName = "New Wave Config")]
-public class WaveConfig : ScriptableObject
+public class WaveConfigSO : ScriptableObject
 {
     [SerializeField] Transform pathPrefab;
     [SerializeField] float moveSpeed = 5f;
